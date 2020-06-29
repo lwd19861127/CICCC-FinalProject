669ff21dc766824d6819e224f9e3116c280c2d94
WendaLi <lwd19861127@hotmail.com>
2020-06-29 10:00:11 +0800

    add Utils

---
+++ LittleSkyGreatGound/Utils/ColorTheme.swift
@@ -0,0 +1,14 @@
+//
+//  ColorTheme.swift
+//  SushiLover
+//
+//  Created by Derrick Park on 5/21/20.
+//  Copyright © 2020 Derrick Park. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let backgroundColor = UIColor(named: "backgroundColor")
+    static let backgroundColorOpposite = UIColor(named: "backgroundColorOpposite")
+}
